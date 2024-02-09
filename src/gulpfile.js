a9463d82b2fd23f0aea6565e9cf03ddcd867c8c0
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-02-10 06:56:11 +0900

    Change path from gulpfile .s

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,9 +4,9 @@ import replace from "gulp-replace";
 console.log("gulp", gulp);
 console.log("replace", replace);
 
-// gulp.task("replace", function () {
-//   return gulp
-//     .src(["./path/to/your/html/files/*.html"])
-//     .pipe(replace("./public/img/", "./assets/"))
-//     .pipe(gulp.dest("./dist"));
-// });
+gulp.task("replace", function () {
+  return gulp
+    .src(["./index.html"])
+    .pipe(replace("./public/img/", "./assets/"))
+    .pipe(gulp.dest("./dist"));
+});
