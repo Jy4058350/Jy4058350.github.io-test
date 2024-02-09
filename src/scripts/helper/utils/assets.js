a5629dc64014c2de728172f50ef4286064f4302b
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-02-09 20:21:17 +0900

    Use getAttribute to get the value of the data-tex-1 attribute

--- src/scripts/helper/utils/assets.js
+++ src/scripts/helper/utils/assets.js
@@ -2,7 +2,8 @@ import image1 from "../../../dist/img/sakuramich.jpeg";
 import image2 from "../../../dist/img/yamadera.jpeg";
 
 export default function init() {
-  const divElement = document.querySelector("[data-webgl]");
-  console.log("divElement", divElement);
-  console.log(divElement.dataset);
-}
+    const divElement = document.querySelector("[data-webgl]");
+    console.log("divElement", divElement);
+    console.log(divElement.getAttribute('data-tex-1')); // Access data-tex-1
+    console.log(divElement.getAttribute('data-tex-2')); // Access data-tex-2
+  }
