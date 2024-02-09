4d211560e2eb1fafe5f7f11b164fea9c99aa9659
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-02-09 19:19:06 +0900

    Create new file named assets.js in helper/utils folder. and check log

---
+++ src/scripts/helper/utils/assets.js
@@ -0,0 +1,7 @@
+import image1 from "../../assets/ima/sakuramich.jpg";
+import image2 from "../../assets/ima/yamadera.jpg";
+
+console.log("manage assets called");
+// const imgElement = document.querySelector('.img[data-webgl]');
+// imgElement.src = image1;
+// imgElement.dataset.tex2 = image2;
