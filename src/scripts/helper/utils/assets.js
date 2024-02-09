bb9dd7720cae584afc1840d53719f71c1b8d27e6
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-02-09 20:10:02 +0900

    I cant get dataset.tex1

--- src/scripts/helper/utils/assets.js
+++ src/scripts/helper/utils/assets.js
@@ -4,5 +4,5 @@ import image2 from "../../../dist/img/yamadera.jpeg";
 export default function init() {
   const divElement = document.querySelector("[data-webgl]");
   console.log("divElement", divElement);
-  console.log(divElement.dataset.tex1);
+  console.log(divElement.dataset);
 }
