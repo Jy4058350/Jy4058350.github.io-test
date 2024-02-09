c05b70c18b7d2d163d80fe56a0a769a220c5b7ea
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-02-10 06:44:51 +0900

    Add newfile named gulpfile.js to assets path

---
+++ src/gulpfile.js
@@ -0,0 +1,11 @@
+// gulpfile.js
+const gulp = require("gulp");
+const replace = require("gulp-replace");
+console.log("gulp", gulp);
+
+// gulp.task("replace", function () {
+//   return gulp
+//     .src(["./path/to/your/html/files/*.html"])
+//     .pipe(replace("./public/img/", "./assets/"))
+//     .pipe(gulp.dest("./dist"));
+// });
