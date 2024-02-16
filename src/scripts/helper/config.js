497aad15abf5b6f1dbff42448178b1b6d130da32
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-02-16 16:36:40 +0900

    Verified commit 7bdcc48

---
+++ src/scripts/helper/config.js
@@ -0,0 +1,12 @@
+export const config = {
+  viewBox: "0 0 100 80",
+  width: "30",
+  height: "30",
+  rectWidth: "100",
+  rectHeight: "10",
+  selector: "button.Header__Icon",
+
+  imageSelector: ".Header__LogoImage",
+  buttonSelector: ".Header__FlexItem",
+  svgElement: ".Icon--cart-desktop",
+};
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,23 +1,69 @@
+import { config } from "../scripts/helper";
 export default (function () {
   adjustGlobalContainer();
+  createSvgIcon(config);
+  adjustImageAndCartToButtonHeight({
+    imageSelector: config.imageSelector,
+    buttonSelector: config.buttonSelector,
+    svgElement: config.svgElement,
+  });
+})();
 
+function adjustParentHeightToChildHeigh(cart, newWidth, buttonHeight) {
+  const parentElement = cart.parentNode;
+  parentElement.style.height = `${buttonHeight}px`;
+  parentElement.style.width = `${newWidth}px`;
+}
+
+function adjustSvgHeightToButtonHeight(cart, buttonHeight) {
+  const bbox = cart.getBBox();
+  const height = bbox.width;
+  const width = bbox.height;
+
+  const aspectRatio = width / height;
+  const newHeight = buttonHeight;
+  const newWidth = aspectRatio * newHeight;
+  cart.style.width = `${newWidth}px`;
+  cart.style.height = `${buttonHeight}px`;
+  adjustParentHeightToChildHeigh(cart, newWidth, buttonHeight);
+}
+
+function adjustImageAndCartToButtonHeight({ imageSelector, buttonSelector, svgElement }) {
+  const button = document.querySelector(buttonSelector);
+  const logoImage = document.querySelectorAll(imageSelector);
+  const cart = document.querySelector(svgElement);
+
+  const buttonHeight = button.offsetHeight;
+
+  logoImage.forEach((logoImage) => {
+    const width = logoImage.offsetWidth;
+    const height = logoImage.offsetHeight;
+
+    const aspectRatio = width / height;
+    const newWidth = aspectRatio * buttonHeight;
+    logoImage.style.width = `${newWidth}px`;
+    logoImage.style.height = `${buttonHeight}px`;
+  });
+  adjustSvgHeightToButtonHeight(cart, buttonHeight);
+}
+function createSvgIcon({ viewBox, width, height, rectWidth, rectHeight, selector }) {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("viewBox", "0 0 100 80");
-  svg.setAttribute("width", "30");
-  svg.setAttribute("height", "30");
+  svg.setAttribute("viewBox", viewBox);
+  svg.setAttribute("width", width);
+  svg.setAttribute("height", height);
 
   for (let i = 0; i < 3; i++) {
     const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    rect.setAttribute("width", "100");
-    rect.setAttribute("height", "10");
-    rect.setAttribute("y", i * 30);
+    rect.setAttribute("width", rectWidth);
+    rect.setAttribute("height", rectHeight);
+    rect.setAttribute("y", i * parseInt(height));
     svg.appendChild(rect);
   }
 
-  const iconWrapper = document.querySelector("button.Header__Icon");
+  const iconWrapper = document.querySelector(selector);
 
   iconWrapper.appendChild(svg);
-})();
+}
 
 function adjustGlobalContainer() {
   const allElements = document.body.getElementsByTagName("*");
@@ -25,7 +71,6 @@ function adjustGlobalContainer() {
   const sectionElements = Array.from(allElements).filter((el) => {
     return el.id.includes("section");
   });
-  console.log(sectionElements);
   const height = new Map();
   sectionElements.forEach((el) => {
     height.set(el.id, el.offsetHeight);
