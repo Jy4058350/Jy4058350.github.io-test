2f16f806ec196e50e7a35113535700cba48c8cd4
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-03-13 05:48:01 +0900

    feat: simple sidebar implementation

--- src/scripts/bootstrap.js
+++ src/scripts/bootstrap.js
@@ -4,7 +4,7 @@ import mouseInit from "./component/mouse.js";
 import { viewport } from "./helper/viewport.js";
 import worldInit, { world } from "./glsl/world.js";
 import loader from "./component/loader.js";
-import {init as stylesInit } from "../styles/styles.js"; //ここのimportは現時点は必要です。今後修正される可能性があります。
+import { init as stylesInit } from "../styles/styles.js"; //ここのimportは現時点は必要です。今後修正される可能性があります。
 
 export default async function init() {
   stylesInit();
@@ -20,5 +20,4 @@ export default async function init() {
   mouseInit();
   world.render();
   loader.letsBegin();
-
 }
